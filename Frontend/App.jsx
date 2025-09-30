bae36aea30055da8f5ba2e7a1e2a29c0d5c286e3
thaovyvy85 <143424790+thaovyvy85@users.noreply.github.com>
2025-09-30 07:31:03 +0700

    Add files via upload

---
+++ Frontend/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://youtu.be/fOQ_-gZsnYQ?list=RDfOQ_-gZsnYQ&t=42" target="_blank">
+          Hello world!
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
